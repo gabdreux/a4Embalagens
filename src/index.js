a49954f8d06131636f411a7dd5fe2e682a62a450
dreuxGab <dev.dreux@hotmail.com>
2024-10-01 16:30:27 -0300

    estrutura básica + setup Firebase Hosting

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// import './styles.css';
+
+// Criar uma raiz com o método createRoot
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// Renderizar o componente App
+root.render(<App />);
